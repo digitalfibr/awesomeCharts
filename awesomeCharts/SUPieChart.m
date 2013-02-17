5dca838004e8e7076d74f0b8ff7dec84cb7b194d
Fabien Charbit <fabiencharbit@MacBook-Pro-de-Fabien-2.local>
2013-02-17 23:16:34 +0100

    add legend

--- awesomeCharts/SUPieChart.m
+++ awesomeCharts/SUPieChart.m
@@ -23,19 +23,37 @@
 
 - (void)drawRect:(NSRect)dirtyRect
 {
+    /******** CONFIG *********/
     //// Color Declarations
-    NSColor* color_1 = [NSColor blueColor];
-    NSColor* color_2 = [NSColor redColor];
-    NSColor* color_3 = [NSColor greenColor];
-    NSColor* color_4 = [NSColor yellowColor];
+    NSColor* color_1 = [NSColor colorWithCalibratedRed:181/255.0
+                              green:224/255.0
+                               blue:115/255.0
+                              alpha:1];
+    NSColor* color_2 = [NSColor lightGrayColor];
+    NSColor* color_3 = [NSColor colorWithCalibratedRed:100/255.0
+                                                 green:220/255.0
+                                                  blue:150/255.0
+                                                 alpha:1];
+    NSColor* color_4 = [NSColor colorWithCalibratedRed:150/255.0
+                                                          green:200/255.0
+                                                           blue:150/255.0
+                                                          alpha:1];
     NSColor* color_5 = [NSColor grayColor];
     NSColor* color_6 = [NSColor purpleColor];
     NSColor* color_7 = [NSColor blackColor];
     NSColor* color_8 = [NSColor blueColor];
     
     NSArray *colorArray = [NSArray arrayWithObjects:color_1,color_2,color_3,color_4,color_5,color_6,color_7,color_8, nil];
+    
+    // values
+    NSArray *values = [NSArray arrayWithObjects:[NSNumber numberWithInt:30], [NSNumber numberWithInt:60],[NSNumber numberWithInt:80],[NSNumber numberWithInt:10], nil];
+    
+    // legend
+    NSArray *keys = [NSArray arrayWithObjects:@"legend1",@"legend2",@"legend3",@"legend4", nil];
 
     NSColor* strokeColor = [NSColor colorWithCalibratedRed: 1 green: 1 blue: 1 alpha: 1];
+    
+    /*************************/
     
     // oval size;
     CGFloat ovalSize = (dirtyRect.size.width<dirtyRect.size.height)?dirtyRect.size.width*0.7:dirtyRect.size.height*0.7;
@@ -44,14 +62,11 @@
     
     NSRect ovalRect = NSMakeRect(ovalPositionX, ovalPositionY, ovalSize, ovalSize);
     
-    // values
-    NSArray *values = [NSArray arrayWithObjects:[NSNumber numberWithInt:30], [NSNumber numberWithInt:60],[NSNumber numberWithInt:80],[NSNumber numberWithInt:10], nil];
-    
     // compute total
     CGFloat total = 0;
     
     for (NSNumber *value in values)
-        total = total + [value floatValue];
+        if ([value floatValue]>0) total = total + [value floatValue];
 
     float startAngle = 180;
     int valueIndex = 0;
@@ -81,14 +96,39 @@
         valueIndex++;
     }
     
-    //// Rectangle Drawing
-    CGFloat legendPositionX = dirtyRect.size.width - (dirtyRect.size.width * 0.2);
-    CGFloat legendPositionY = dirtyRect.size.height - (dirtyRect.size.height * 0.2);
+/***    legend   ***/
     
-    NSColor *fillColor = [colorArray objectAtIndex:0];
-    NSBezierPath* rectanglePath = [NSBezierPath bezierPathWithRect: NSMakeRect(legendPositionX, legendPositionY, 20, 20)];
-    [fillColor setFill];
-    [rectanglePath fill];
+    //// Rectangle Drawing
+    CGFloat legendPositionX = ovalSize + (dirtyRect.size.width * 0.05);
+    CGFloat legendPositionY = dirtyRect.size.height - (dirtyRect.size.height * 0.3);
+    CGFloat legendSquareSize = 10;
+    valueIndex = 0;
+    
+    for (NSString *key in keys){
+        
+        NSColor *fillColor = [colorArray objectAtIndex:valueIndex];
+        NSBezierPath* rectanglePath = [NSBezierPath bezierPathWithRect: NSMakeRect(legendPositionX, legendPositionY, legendSquareSize, legendSquareSize)];
+        [fillColor setFill];
+        [rectanglePath fill];
+        
+        //// Text Drawing
+        NSString* textContent = @"Hello, World!";
+        NSRect textRect = NSMakeRect(legendPositionX + legendSquareSize, legendPositionY, 100, 16);
+        NSColor *textColor = [NSColor blackColor];
+        
+        NSMutableParagraphStyle* textStyle = [[NSMutableParagraphStyle defaultParagraphStyle] mutableCopy];
+        [textStyle setAlignment: NSCenterTextAlignment];
+        
+        NSDictionary* textFontAttributes = [NSDictionary dictionaryWithObjectsAndKeys:
+                                            [NSFont fontWithName: @"Helvetica" size: 12], NSFontAttributeName,
+                                            textColor, NSForegroundColorAttributeName,
+                                            textStyle, NSParagraphStyleAttributeName, nil];
+        
+        [textContent drawInRect: textRect withAttributes: textFontAttributes];
+        
+        legendPositionY = legendPositionY - legendSquareSize - 5;
+        valueIndex++;
+    }
 }
 
 @end
